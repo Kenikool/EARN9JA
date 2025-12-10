b6533265646c0e5a1d58d1925719eaea2485ec03
Kenikool <kenikoolchaawa070@gmail.com>
2025-12-10 14:03:22 +0100

    resend

---
+++ build-earn9ja-android.bat
@@ -0,0 +1,4 @@
+@echo off
+cd "Earn9ja\Earn9ja"
+eas build --platform android --profile production
+pause
--- vidflow-backend/install.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Installing VidFlow Backend Dependencies...
-npm install
-echo.
-echo Installation complete!
-echo.
-echo To start the server, run: npm start
-pause
